4dac98a12a25c1d813f1d58c55a85c4a6bf455d5
Dmytro Chumachenko <dmytro.chumachenko@nixsolutions.com>
2023-05-25 15:50:00 +0200

    change username to sub

--- screenshot_api/routers/auth.py
+++ screenshot_api/routers/auth.py
@@ -4,7 +4,10 @@ import boto3
 import requests
 from fastapi import APIRouter
 from fastapi.responses import RedirectResponse
+from starlette.exceptions import HTTPException
 from starlette.requests import Request
+
+from utils.auth import get_sub
 
 auth_router = APIRouter()
 
@@ -28,31 +31,36 @@ def get_token(code: str):
     idp = boto3.client("cognito-idp")
     client_id = idp.list_user_pool_clients(UserPoolId=os.getenv("UserPoolId"))["UserPoolClients"][0]["ClientId"]
     client = idp.describe_user_pool_client(UserPoolId=os.getenv("UserPoolId"), ClientId=client_id)
-    token = requests.post(
-        f"https://{os.getenv('CognitoDomain')}.auth.{os.getenv('Region')}.amazoncognito.com/oauth2/token",
-        data={
-            "grant_type": "authorization_code",
-            "code": code,
-            "client_id": client_id,
-            "redirect_uri": client["UserPoolClient"]["CallbackURLs"][0],
-        }
-    ).json()
-    username = idp.get_user(AccessToken=token["access_token"])["Username"]
+    try:
+        token = requests.post(
+            f"https://{os.getenv('CognitoDomain')}.auth.{os.getenv('Region')}.amazoncognito.com/oauth2/token",
+            data={
+                "grant_type": "authorization_code",
+                "code": code,
+                "client_id": client_id,
+                "redirect_uri": client["UserPoolClient"]["CallbackURLs"][0],
+            }
+        ).json()
+        sub = get_sub(idp.get_user(AccessToken=token["access_token"]))
+    except (idp.exceptions.NotAuthorizedException, KeyError):
+        raise HTTPException(status_code=400, detail="Invalid token")
 
     dynamo_db = boto3.client('dynamodb')
+    user_table = os.getenv("UserTable")
+
     if not dynamo_db.get_item(
-            TableName=os.getenv("UserTable"),
+            TableName=user_table,
             Key={
                 "id": {
-                    "S": username,
+                    "S": sub,
                 }
             }
     ).get("Item"):
         dynamo_db.put_item(
-            TableName=os.getenv("UserTable"),
+            TableName=user_table,
             Item={
                 "id": {
-                    "S": username,
+                    "S": sub,
                 },
                 "subscription": {
                     "N": "1",
