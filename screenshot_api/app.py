64252bca11a86f523b72e7c159f6bbc8d2bd0986
Dmytro Chumachenko <dmytro.chumachenko@nixsolutions.com>
2023-06-08 13:31:28 +0200

    add root_path

--- screenshot_api/app.py
+++ screenshot_api/app.py
@@ -1,9 +1,10 @@
 from fastapi import FastAPI
 from mangum import Mangum
 
+from config import settings
 from routers.auth import auth_router
 
-app = FastAPI()
+app = FastAPI(root_path=f"/{settings.stack_name}")
 app.include_router(auth_router)
 
 lambda_handler = Mangum(app)
