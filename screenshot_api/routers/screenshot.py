9f26e8cca4e184fee44cb5c37733107067eb26c1
Dmytro Chumachenko <dmytro.chumachenko@nixsolutions.com>
2023-06-09 15:53:38 +0300

    add ability to upload files to S3

---
+++ screenshot_api/routers/screenshot.py
@@ -0,0 +1,35 @@
+import os
+from typing import Annotated
+
+import boto3
+from boto3.exceptions import S3UploadFailedError
+from fastapi import APIRouter, Depends, UploadFile, HTTPException
+
+from config import settings
+from models import User
+from utils.auth import get_current_user
+
+screenshot_router = APIRouter()
+
+
+@screenshot_router.post("/upload_file")
+async def upload_file(current_user: Annotated[User, Depends(get_current_user)], file: UploadFile):
+    """
+    method to store file locally, then upload to s3 bucket
+    """
+    s3 = boto3.resource("s3")
+    bucket = s3.Bucket(settings.bucket_name)
+
+    with open("/tmp/" + file.filename, "wb") as local_file:
+        local_file.write(await file.read())
+
+    try:
+        bucket.upload_file("/tmp/" + file.filename, current_user.id + "/" + file.filename)
+    except S3UploadFailedError as e:
+        raise HTTPException(status_code=400, detail=str(e))
+    finally:
+        os.remove("/tmp/" + file.filename)
+
+    return {
+        "msg": f"{file.filename} successfully uploaded"
+    }
